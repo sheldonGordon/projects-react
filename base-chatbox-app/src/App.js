e969c5234de150451e2d8558108a612cb09a3bcc
aurélien <achatelain62000@gmail.com>
2020-02-10 21:28:14 +0100

    second commit

---
+++ base-chatbox-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='box'>
+        <h1>Titre</h1>
+      </div>
+    )
+  }
+}
+
+export default App
